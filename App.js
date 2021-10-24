cdcc70d72c4b9a91b36cf28f7dff3998b032a9fd
Jason Park <zeipar@gmail.com>
2021-10-24 13:02:30 -0700

    Added color theme props. Created basic header component with two touchable buttons.

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { theme } from './colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Text style={styles.btnTxt}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.btnTxt}>Travel</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  btnTxt: {
+    color: "white",
+    fontSize: 35,
+    fontWeight: "600",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
   },
+  header: {
+    flexDirection: 'row',
+    justifyContent:'space-between',
+    marginTop: 50,
+  }
 });
